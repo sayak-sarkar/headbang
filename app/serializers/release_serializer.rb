b85f0b7752f1011f6c371ce400cf334a1bfc8112
Victor Martinez <knoopx@gmail.com>
2014-05-31 16:39:44 +0200

    Return release play_count

--- app/serializers/release_serializer.rb
+++ app/serializers/release_serializer.rb
@@ -1,5 +1,5 @@
 class ReleaseSerializer < ActiveModel::Serializer
-  attributes :id, :title, :artist, :tracks_count, :artwork_url, :collection_ids, :year, :path, :name
+  attributes :id, :title, :artist, :tracks_count, :play_count, :artwork_url, :collection_ids, :year, :path, :name
 
   has_one :artist, :release
 
